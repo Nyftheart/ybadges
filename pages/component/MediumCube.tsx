f59fb4e86ef26f3e6a37b952bd13d50424f38464
Nyftheart <alexandre.kramer@ynov.com>
2022-12-29 17:55:45 +0100

    add desgin

--- pages/component/MediumCube.tsx
+++ pages/component/MediumCube.tsx
@@ -1,7 +1,37 @@
 import React from 'react';
+import {FaChartBar} from "react-icons/fa";
 const MediumCube = () =>{
     return (
-        <div className="flex ml-60 mt-4 text-2xl mt-11 justify-center items-center bg-white drop-shadow-md w-80 h-40 rounded-2xl hover:scale-105 duration-300">
+        <div className="ml-64 mt-4 text-2xl mt-11 mr-96 justify-center items-center bg-white drop-shadow-md h-52 rounded-2xl hover:scale-105 duration-300">
+            <div className="flex">
+                <FaChartBar className="text-slate-500 ml-10 mt-4"/>
+                <p className="pl-3 pt-3 text-slate-500 text-xl">Cette Semaine</p>
+            </div>
+
+            <div className="flex justify-between px-20 pt-10 text-center">
+                <div className="flex">
+                    <div>
+                        <h5>150</h5>
+                        <p className="text-sm mt-3 text-slate-400 text-center">Demandes</p>
+                    </div>
+
+                    <hr className="separation"/>
+                </div>
+                <div className="flex">
+                    <div>
+                        <h5>150</h5>
+                        <p className="text-sm mt-3 text-slate-400 text-center">Demandes</p>
+                    </div>
+
+                    <hr className="separation"/>
+                </div>
+                <div className="flex">
+                    <div>
+                        <h5>150</h5>
+                        <p className="text-sm mt-3 text-slate-400 text-center">Demandes</p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
